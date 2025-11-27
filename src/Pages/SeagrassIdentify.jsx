62d7a02ef50a0f05d850ba3ff9bc886c57177b19
Pasan Ramyanath <pjramyanath@gmail.com>
2025-11-27 09:09:32 +0530

    Update SeagrassIdentify.jsx

--- src/Pages/SeagrassIdentify.jsx
+++ src/Pages/SeagrassIdentify.jsx
@@ -55,7 +55,7 @@ const SeagrassIdentify = () => {
     formData.append("image", image);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://seagrass-identify.agreeablepond-f717735e.southindia.azurecontainerapps.io/predict", {
         method: "POST",
         body: formData,
       });
